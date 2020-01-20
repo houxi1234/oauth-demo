0964cf9f24f90d141cba09fc180630c64386242c
562066559@qq.com <5922644houxi>
2020-01-20 10:45:06 +0800

    submit

--- sso-auth-client-a/src/main/java/com/hx/oauth/resource/ClientAApplication.java
+++ sso-auth-client-a/src/main/java/com/hx/oauth/resource/ClientAApplication.java
@@ -8,7 +8,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ClientAApplication {
 
     public static void main(String[] args) {
